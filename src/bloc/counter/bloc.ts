9bce157778f5669b3ed490fd69a0e8f000795db1
kim <k01084758975@gmail.com>
2023-06-17 00:26:13 +0900

    refactor

--- src/bloc/counter/bloc.ts
+++ src/bloc/counter/bloc.ts
@@ -3,7 +3,6 @@ import { CounterState, CounterStatus } from "./state";
 import { IncrementEvent, DecrementEvent, CounterEvent } from "./event";
 
 export const [state, setState] = createSignal<CounterState>(new CounterState(CounterStatus.Initial, 0))
-export const [event, setEvent] = createSignal<CounterEvent | null>(null)
 
 export default class CounterBloc {
     private constructor() { }
@@ -12,13 +11,7 @@ export default class CounterBloc {
         return state()
     }
 
-    static get eventGetter() {
-        return event()
-    }
-
     static eventSink(e: CounterEvent) {
-        setEvent(e);
-
         if (e instanceof IncrementEvent) e.increment()
         else if (e instanceof DecrementEvent) e.decrement()
     }
--- src/comps/Counter.tsx
+++ src/comps/Counter.tsx
@@ -1,13 +1,14 @@
 import { Match, Switch } from "solid-js"
 import CounterBloc, { e } from "../bloc/counter/bloc"
 import { DecrementEvent, IncrementEvent } from "../bloc/counter/event"
+import { CounterStatus } from "../bloc/counter/state"
 
 export default function Counter() {
     return (
         <>
             <Switch fallback={ <div>no State</div> }>
-                <Match when={ CounterBloc.eventGetter instanceof IncrementEvent }>state: added</Match>
-                <Match when={ CounterBloc.eventGetter instanceof DecrementEvent }>state: substracted</Match>
+                <Match when={ CounterBloc.stateGetter.status == CounterStatus.Added }>state: added</Match>
+                <Match when={ CounterBloc.stateGetter.status == CounterStatus.Substract }>state: substracted</Match>
             </Switch>
             <div>{ CounterBloc.stateGetter.counterValue }</div>
 
