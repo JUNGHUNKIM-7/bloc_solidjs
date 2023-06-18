0f6719972cfa188be444b4684531c07cd4f0c35a
kim <k01084758975@gmail.com>
2023-06-18 15:38:17 +0900

    add ref

--- src/comps/Store2.tsx
+++ src/comps/Store2.tsx
@@ -13,16 +13,17 @@ const [category, setCategory] = createStore<Category>({} as Category)
 
 export default function MoreComp() {
 	let inputRef!: HTMLInputElement
+	let inputRef2!: HTMLInputElement
 
 	function updateSub() {
 		setCategory('sub', (prev) => ({ ...prev, name: inputRef.value }))
 	}
 
-	function insertTag(s: string) {
-		setCategory('tags', (prev) => [...prev ?? [], s])
+	function insertTag() {
+		setCategory('tags', (prev) => [...prev ?? [], inputRef2.value])
 		setCategory(produce((category) => {
 			category.sub.name = 'hi'
-			;(category.tags ?? []).push(s)
+			;(category.tags ?? []).push(inputRef2.value)
 		}))
 	}
 
@@ -30,8 +31,8 @@ export default function MoreComp() {
 		<div>
 			<input ref={inputRef} type='text' name='sub' id='sub' />
 			<button onClick={updateSub}>update subcategory</button>
-			<input type='text' name='tag' id='tag' />
-			<button onClick={() => insertTag('hi')}>insert tag</button>
+			<input ref={inputRef2} type='text' name='tag' id='tag' />
+			<button onClick={insertTag}>insert tag</button>
 			<div>
 				{JSON.stringify(category, null, 2)}
 			</div>
